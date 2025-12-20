c8ac1208d95114d658651f487264bddabd9a595c
ROIEngineer <191797684+ROIEngineer@users.noreply.github.com>
2025-12-20 01:01:28 -0500

    Snake Body + Direction Control
    A real snake (multiple segments)
    
    Keyboard controls (⬆️⬇️⬅️➡️)
    
    Clean movement logic (no diagonals)

--- game.js
+++ game.js
@@ -1,24 +1,46 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const TILE_SIZE = 20;
+const TILE_SIZE = 10;
 const CANVAS_SIZE = 400;
 
-let x = 0;
-let y = 0;
+let snake = [
+  { x: 200, y: 200 },
+  { x: 180, y: 200 },
+  { x: 160, y: 200 }
+];
+
+let direction = "UP";
 
 function update() {
-  x += TILE_SIZE; // Move right by one tile
-  if (x >= CANVAS_SIZE) { // Is edge reached?
-    x = 0; // Reset to left side
+  const head = { ...snake[0] }; // Array > list of items
+
+  switch (direction) {
+    case "UP":
+      head.y -= TILE_SIZE;
+      break
+    case "DOWN":
+      head.y += TILE_SIZE;
+      break
+    case "LEFT":
+      head.x -= TILE_SIZE;
+      break
+    case "RIGHT":
+      head.x += TILE_SIZE;
+      break
   }
+
+  snake.unshift(head); // Add new head to beginning
+  snake.pop(); // Removes last item from the end
 }
 
 function draw() {
   ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE); // Clear the previous frame
 
   ctx.fillStyle = "limegreen"; // Set color
-  ctx.fillRect(x, y, TILE_SIZE, TILE_SIZE); // Draw square at current position
+  snake.forEach((segment) => {
+    ctx.fillRect(segment.x, segment.y, TILE_SIZE, TILE_SIZE);
+  });
 }
 
 function gameLoop() {
@@ -27,3 +49,20 @@ function gameLoop() {
 }
 
 setInterval(gameLoop, 200);
+
+document.addEventListener("keydown", e => {
+  switch (e.key) {
+    case "ArrowUp":
+      if (direction !== "DOWN") direction = "UP";
+      break;
+    case "ArrowDown":
+      if (direction !== "UP") direction = "DOWN";
+      break;
+    case "ArrowLeft":
+      if (direction !== "RIGHT") direction = "LEFT";
+      break;
+    case "ArrowRight":
+      if (direction !== "LEFT") direction = "RIGHT";
+      break;
+  }
+});
